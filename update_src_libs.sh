8d65af8bc00aa600fc966b78307a6f09eb090325
Engelbert Niehaus <niehaus@uni-landau.de>
2019-09-17 12:16:03 +0200

    unused update files removed

--- update_src_libs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget https://niebert.github.io/icons4menu/wget_icons.sh -O ./wget_icons.sh
-wget https://niebert.github.io/jsoneditor4code/js/jsoneditor4code.js -O ./js/jsoneditor4code.js
